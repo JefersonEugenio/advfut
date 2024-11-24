8412d608b9cd2f579d68b1efdcb8a2cdc9ddadcb
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-11-24 12:03:02 -0300

    Menu notificaação numero lida e desparece lida

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,9 +13,10 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
         <!-- CSS da aplicação -->
-
+        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
         <link rel="stylesheet" href="/css/styles.css">
         <script src="/js/scripts.js"></script>
+        
     </head>
     <body>
         <header>
@@ -46,10 +47,14 @@
                         </li>
                         <li class="nav-item">
                         <a href="/notifications" class="nav-link">
-                            Notificações
-                            @if(auth()->user()->unreadNotifications->count() > 0)
-                                <span class="badge badge-danger">{{ auth()->user()->unreadNotifications->count() }}</span>
-                            @endif
+                            <?php $noti = auth()->user()->unreadNotifications->count(); ?>
+                                Notificações
+                                @php
+                                    $noti = auth()->user()->unreadNotifications->count();
+                                @endphp
+                                @if($noti > 0)
+                                    <span class="badge badge-danger" style="color: white; background-color: red;">{{ $noti }}</span>
+                                @endif
                         </a>
                         </li>
                         <li class="nav-item">
