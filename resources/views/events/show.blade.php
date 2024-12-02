8c272ad1d786228c0ea5ebde299ee127ec0453b3
Cleverson Sampaio <clvrsnsampaio@outlook.com>
2024-12-02 15:04:12 -0300

    Atualização da pagina do evento para mobile

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -7,64 +7,66 @@
             <x-header />
         </div>
 
-        <div class="w-full flex items-center justify-center">
-            <div class="w-full max-w-7xl py-5 flex flex-col sm:flex-row gap-3">
-                <div id="image-container" class="col-md-4 d-flex justify-content-center align-items-center">
-                    <img src="/img/events/{{ $equipe->imagem }}" class="img-fluid" alt="{{ $equipe->clube }}">
-                </div>
+        <div class="m-5 flex flex-col sm:flex-row items-start justify-between gap-3">
+            <div class="w-full flex flex-col">
+                <div class="w-full flex items-center justify-center">
+                    <div class="w-full max-w-7xl p-3 border border-gray-400 rounded-md flex flex-col gap-3">
+                        <div class="w-full flex flex-col sm:flex-row gap-4">
+                            <div id="image-container" class="bg-cover bg-center w-full sm:w-60 h-60"
+                                style="background-image:url(<?= '/img/events/' . $equipe->imagem ?>)"></div>
+                            <div id="info-container" class="col-md-4">
+                                <a href="/teams/{{ $equipe->id }}" class="text-xl">{{ $equipe->clube }}</a>
+                                <p class="text-gray-500">Proprietário: {{ $eventOwner->name }}</p>
+                                <p class="event-date">Data: {{ date('d/m/y', strtotime($agenda->data)) }}</p>
+                                <p class="event-horario">Hora: {{ \Carbon\Carbon::parse($agenda->hora)->format('H:i') }}</p>
+                                <p class="event-dure">Duração: {{ \Carbon\Carbon::parse($agenda->duracao)->format('H:i') }}
+                                </p>
+                                <p class="event-quadras">Tipo de jogo: {{ $agenda->tipo }}</p>
+                                <div class="event-local">
+                                    <p>Endereço:</p>
+                                    <p class="underline">{{ $agenda->endereco }}, {{ $agenda->bairro }} - {{ $agenda->cidade }}</p>
+                                </div>
+                                <p class="event-pagamento">Pagamento: {{ $agenda->pagamento }}</p>
+                            </div>
+                        </div>
+                        
+                        <div class="flex flex-col gap-1">
+                            <h3>Descrição sobre o adversário:</h3>
+                            <p class="event-description">{{ $agenda->observacao ? $agenda->observacao : "Sem descrição" }}</p>
+                        </div>
 
-                <div id="info-container" class="col-md-4">
-                    <p class="event-club"><ion-icon name="body-outline"></ion-icon> {{ $eventOwner->name }} </p>
-                    <a href="/teams/{{ $equipe->id }}"><ion-icon name="shield-outline"></ion-icon>{{ $equipe->clube }}</a>
-                    <p class="event-date"><ion-icon name="calendar-number-outline"></ion-icon>
-                        {{ date('d/m/y', strtotime($agenda->data)) }}</p>
-                    <p class="event-horario"><ion-icon name="alarm-outline"></ion-icon>
-                        {{ \Carbon\Carbon::parse($agenda->hora)->format('H:i') }} </p>
-                    <p class="event-dure"><ion-icon name="stopwatch-outline"></ion-icon>
-                        {{ \Carbon\Carbon::parse($agenda->duracao)->format('H:i') }} </p>
-                    <p class="event-quadras"><ion-icon name="football-outline"></ion-icon> {{ $agenda->tipo }} </p>
-                    <p class="event-local"><ion-icon name="home-outline"></ion-icon> {{ $agenda->endereco }} </p>
-                    <p class="event-bairro"><ion-icon name="trail-sign-outline"></ion-icon> {{ $agenda->bairro }} </p>
-                    <p class="event-city"><ion-icon name="location-outline"></ion-icon> {{ $agenda->cidade }} </p>
-                    <p class="event-pagamento"><ion-icon name="cash-outline"></ion-icon> {{ $agenda->pagamento }} </p>
-                    @if (auth()->check())
-                        @if ($agenda->equipe_adversario == null)
-                            <form action="/events/join/{{ $agenda->id }}" method="POST">
-                                @csrf
-                                <div>
-                                    <label for="equipe_id">Escolhe seu time:</label>
-                                    <select name="equipe_id" id="equipe_id" class="form-control" required>
+                        @if (auth()->check())
+                            @if ($agenda->equipe_adversario == null)
+                                <form action="/events/join/{{ $agenda->id }}" method="POST" class="flex flex-col sm:flex-row items-end justify-end gap-3">
+                                    @csrf
+                                    <select name="equipe_id" id="equipe_id" class="w-full sm:w-auto h-12 rounded-md" required>
                                         @foreach ($user->equipes as $equipe)
+                                            <option disabled>Selecione uma equipe</option>
                                             <option value="{{ $equipe->id }}">{{ $equipe->clube }}</option>
                                         @endforeach
                                     </select>
-                                </div>
-                                <button type="submit" class="btn btn-primary" id="event-submit">Confirmar partida</button>
-                            </form>
+                                    <button type="submit" class="bg-orange-400 w-full sm:w-auto px-8 h-12 text-white rounded-md" id="event-submit">Confirmar
+                                        partida</button>
+                                </form>
+                            @else
+                                <p class="already-joined-msg">Esse time já está participando deste jogo contra o
+                                    adversário:
+                                    {{ $agenda->equipeAdversario->clube }}</p>
+                            @endif
                         @else
-                            <p class="already-joined-msg">Esse time já está participando deste jogo contra o adversário:
-                                {{ $agenda->equipeAdversario->clube }}</p>
+                            <p class="login-msg">Para confirmar a participação, -> <a href="/login">faça login</a> <- na
+                                    sua conta.</p>
                         @endif
-                    @else
-                        <p class="login-msg">Para confirmar a participação, -> <a href="/login">faça login</a> <- na sua
-                                conta.</p>
-                    @endif
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="w-full flex items-center justify-center">
-            <div class="w-full max-w-7xl py-5 flex flex-col gap-3" id="description-container">
-                <h3>Descrição sobre o adversário:</h3>
-                <p class="event-description">{{ $agenda->observacao }}</p>
-            </div>
-        </div>
-
-        <div class="w-full flex items-center justify-center">
-            <div class="w-full max-w-7xl py-5 flex flex-row gap-3">
-                <div id="mapa" class="w-full h-[400px]">
-                    <h3>Localização</h3>
-                    <div style="height: 400px; width: 100%; border: 1px solid #ccc;"></div>
+            <div class="w-full sm:max-w-lg flex items-center justify-center">
+                <div class="w-full max-w-7xl flex flex-row gap-3">
+                    <div id="mapa" class="w-full h-[400px]">
+                        <h3>Localização</h3>
+                        <div style="height: 400px; width: 100%; border: 1px solid #ccc;"></div>
+                    </div>
                 </div>
             </div>
         </div>
